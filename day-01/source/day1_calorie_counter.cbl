a4650af9fda4cceed765013d5f56ff2e820b55ba
Aravind Selvakumar <aravind.selvakumar@hotmail.com>
2022-12-05 01:28:56 +0800

    Day 1 - part 2
    Find sum of top three . Day 1 part 2 of the challenge

--- day-01/source/day1_calorie_counter.cbl
+++ day-01/source/day1_calorie_counter.cbl
@@ -12,26 +12,27 @@
       * INPUT FILE - 9 BYTES
        FD  File-In.
        01  CALORIES-IN.
-           05  CALORIES-IN-STOCK              PIC 9(9).
+           05  CALORIES-IN-STOCK              PIC x(5).
 
        WORKING-STORAGE SECTION.
       *VARIABLES
        01 W-Variables.
-           05  w-sum-calories                 pic 9(10).
+           05  w-sum-calories                 pic 9(7).
            05  w-elf-counter                  pic 9(4).    
-           05  w-max-elf.
-               10  w-elf-identifier           pic zzz9.
-               10  w-calories-instock         pic zzzzzzzzz9.
+           05  w-max-elf occurs 500 times.
+               10  w-calories-instock         pic 9(7).
+           05  w-max-sum                      pic 9(10).
+           05  w-subscript                    pic 9(4).
       
       *FLAGS 
-       01 INPUT-FILE-STATUS                  PIC X(1).
-           88 INPUT-FILE-EOF                   VALUE 'Y'.
+       01 INPUT-FILE-STATUS                   pic x(1).
+           88 INPUT-FILE-EOF                   value 'Y'.
 
        Procedure Division.
        0000-Begin Section.
            perform 1000-Initialize 
            perform 2000-Main-Process
-           perform 3000-terminate
+           perform 3000-print-summary
            stop run.
       ******************************************************************
       * Initialize 
@@ -40,12 +41,12 @@
            
            initialize  INPUT-FILE-STATUS
                        w-sum-calories
-                       w-max-elf
                        w-elf-counter
 
            open input File-In
 
            move 1 to w-elf-counter
+           move 1 to w-subscript
            .
       ******************************************************************
       * Initialize 
@@ -53,41 +54,81 @@
        2000-Main-Process section.
            
            perform 2100-Read-File
-      *     
-           perform until INPUT-FILE-EOF
-               
-               if CALORIES-IN-STOCK is > zeroes
-                   compute w-sum-calories = 
-                           w-sum-calories + CALORIES-IN-STOCK
-               else 
-                   if w-calories-instock < w-sum-calories
-                       move w-elf-counter  to w-elf-identifier
-                       move w-sum-calories to w-calories-instock
-                   end-if
-               
-                   initialize w-sum-calories
-                   compute w-elf-counter = w-elf-counter + 1
-               end-if
-
-               perform 2100-Read-File
-                   
-           END-PERFORM
+      *    
+           perform 2200-Load-Table
+           
+           sort w-max-elf DESCENDING w-calories-instock 
            .
 
       ******************************************************************
-      * Initialize 
+      * Read File 
       ******************************************************************
        2100-Read-File section.
            read File-In into CALORIES-IN 
            at end set INPUT-FILE-EOF to true 
            end-read
            .
+      ******************************************************************
+      * Load the key and value to internl table
+      ******************************************************************
+       2200-Load-Table section.
+           
+           perform until INPUT-FILE-EOF
+               
+               if CALORIES-IN-STOCK is > zeroes
+                   compute w-sum-calories = w-sum-calories +
+                            function numval(CALORIES-IN-STOCK)
+               else 
+      
+                    move w-sum-calories 
+                      to w-calories-instock(w-subscript) 
+
+                    compute w-subscript = w-subscript + 1
+                    initialize w-sum-calories
+                      
+               end-if
+      *         compute w-elf-counter = w-elf-counter + 1
+               perform 2100-Read-File
+                   
+           END-PERFORM
+           .
 
       ******************************************************************
-      * Initialize 
+      * Calculate the top three elves with calories in stock 
       ******************************************************************
-       3000-terminate section.
-           display "Max-calories: " w-calories-instock
-           display "Elf-Id:  " w-elf-identifier
+       2200-compute-top-three section.
+
+           initialize w-subscript
+           move 1 to w-subscript
+           perform 3 times 
+               if w-calories-instock(w-subscript) < w-sum-calories
+      *              
+                    move w-sum-calories 
+                      to w-calories-instock(w-subscript)
+               end-if
+               compute w-subscript = w-subscript + 1
+           end-perform
+               
+           initialize w-sum-calories
+                      w-subscript
+           .     
+
+      ******************************************************************
+      * Summary  
+      ******************************************************************
+       3000-print-summary section.
+
+           move 1 to w-subscript
+           perform 3 times 
+               display "Elf " w-subscript 
+               display "Max-calories: " w-calories-instock(w-subscript) 
+               display "-----------------"
+               
+               compute w-max-sum = 
+                       w-max-sum + w-calories-instock(w-subscript)
+               compute w-subscript = w-subscript + 1
+           end-perform
+           
+           display "w-max-sum : " w-max-sum
            close File-In
            .
